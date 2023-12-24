0037175f950254fbcec7e4438387bdc214787b71
TheHeroz <69307158+TheHeroz819@users.noreply.github.com>
2023-12-24 11:55:43 +0530

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,8 @@
 from discord import Intents
+from discord import Embed
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+from discord import Colour
 
 PREFIX=":"
 OWNER_IDS = [987016559794987059]
@@ -40,8 +42,17 @@ class Bot(BotBase):
 			print("Bot ready!")
 
 			channel = self.get_channel(1187725789307863133)
-			await channel.send("Now Online! <@987016559794987059> Sleep now!")
+			await channel.send("Now Online!")
 
+			embed= Embed(title="Now Online!", description="TK's Bot is Online!", colour=0x3D85C6)
+			fields= [("What can I do?", "I can't do anything now!", True),
+			("Why?", "My owner needs to program me!", True),
+			("How long?", "Won't you damn wait for the developer to add commands?", False),
+			("What is the prefix?", "The Prefix is : For Example :help", False)]
+			for name, value, inline in fields:
+				embed.add_field(name=name, value=value, inline=inline)
+			await channel.send(embed=embed)
+			
 		else:
 			print("Bot reconnected")
 
