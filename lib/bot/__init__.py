758b48f3f48900fca577cf75aadbb7716523a548
TheHeroz <69307158+TheHeroz819@users.noreply.github.com>
2023-12-25 12:43:16 +0530

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -3,6 +3,10 @@ from discord import Embed
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
 from discord import Colour
+from datetime import datetime
+from dateutil.tz import gettz
+
+
 
 PREFIX=":"
 OWNER_IDS = [987016559794987059]
@@ -44,13 +48,14 @@ class Bot(BotBase):
 			channel = self.get_channel(1187725789307863133)
 			await channel.send("Now Online!")
 
-			embed= Embed(title="Now Online!", description="TK's Bot is Online!", colour=0x3D85C6)
+			embed= Embed(title="Now Online!", description="TK's Bot is Online!", colour=0x3D85C6, timestamp=datetime.now(tz=gettz('Asia/Kolkata')))
 			fields= [("What can I do?", "I can't do anything now!", True),
 			("Why?", "My owner needs to program me!", True),
 			("How long?", "Won't you damn wait for the developer to add commands?", False),
 			("What is the prefix?", "The Prefix is : For Example :help", False)]
 			for name, value, inline in fields:
 				embed.add_field(name=name, value=value, inline=inline)
+			embed.set_author(name="TokyoTheTheHeroz", icon_url="https://cdn.discordapp.com/avatars/987016559794987059/0c25c9f619475ccfde468db95ab6a137.webp?size=160")
 			await channel.send(embed=embed)
 			
 		else:
